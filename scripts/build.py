05bd006a115c99810c6d0cce96613b49c9ff0588
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2022-02-17 23:48:07 +0100

    fix build script on Windows

--- scripts/build.py
+++ scripts/build.py
@@ -10,20 +10,25 @@ icon = None
 BASE_DIR = Path(__file__).parent.parent
 RESOURCES_DIR = BASE_DIR / 'resources'
 _os = None
+_add_data_separator = None
 
 # platform specific code
 if _platform == "linux" or _platform == "linux2":  # linux
     icon = None
     _os = 'linux'
+    _add_data_separator = ":"
 elif _platform == "darwin":  # MAC OS X
     icon = RESOURCES_DIR / 'icon.icns'
     _os = 'macos'
+    _add_data_separator = ":"
 elif _platform == "win32":  # Windows
     icon = RESOURCES_DIR / 'icon.ico'
     _os = 'wind32'
+    _add_data_separator = ";"
 elif _platform == "win64":  # Windows 64-bit
     icon = RESOURCES_DIR / 'icon.ico'
     _os = 'win64'
+    _add_data_separator = ";"
 
 PyInstaller.__main__.run([
     str(BASE_DIR / f'{name}.py'),
@@ -33,6 +38,6 @@ PyInstaller.__main__.run([
     '--clean',
     '--log-level=WARN',
     f'--name={name}-{_os}-{__version__}',
-    f'--add-data={BASE_DIR / "resources" / "azure"}:{os.path.join("resources", "azure")}',
+    f'--add-data={BASE_DIR / "resources" / "azure"}{_add_data_separator}{os.path.join("resources", "azure")}',
     '-y'
 ])
