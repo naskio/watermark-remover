f0879c82119a670a0f8853336fcd59d99adb5530
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2022-02-17 21:20:59 +0100

    add geofond method

--- main.py
+++ main.py
@@ -3,58 +3,31 @@ from PIL import Image
 from pathlib import Path
 import zlib
 import numpy
-from pikepdf import Pdf, PdfImage, Name
+from pikepdf import Pdf, PdfImage, Name, parse_content_stream, unparse_content_stream
 import cv2
 import zipfile
 import io
 import numpy as np
 # from quality import improve_text_in_image
 import fire
+from enum import Enum
 
 
-def replace_images_in_zip(in_zip: Path, out_zip: Path, replacements: Dict[str, io.BytesIO]) -> Path:
-    """
-    Replace images in zip
-    :param in_zip:
-    :param out_zip:
-    :param replacements:
-    :return:
-    """
+class MethodChoice(Enum):
+    geofond1dot22 = "GEOFOND v1.22"
+    colors_replacement = "Replace colors"
+    openCV2 = "OpenCV2"
 
-    with zipfile.ZipFile(in_zip, 'r') as zin:
-        with zipfile.ZipFile(out_zip, 'w') as zout:
-            zout.comment = zin.comment  # preserve the comment
-            for item in zin.infolist():
-                # copy all files except the images to replace
-                if item.filename not in replacements.keys():
-                    with zout.open(item.filename, 'w') as f:
-                        f.write(zin.read(item.filename))
-                else:  # replace the image
-                    with zout.open(item.filename, 'w') as f:
-                        f.write(replacements[item.filename].getvalue())
-    return out_zip
-
-
-def remove_watermark_from_cv_image(img: numpy.ndarray) -> numpy.ndarray:
-    """
-    Remove watermark from open cv image
-    :param img:
-    :return: image without watermark
-    """
-    # convert image to hsv colorspace
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    h, s, v = cv2.split(hsv)
-    # threshold saturation image
-    thresh1 = cv2.threshold(s, 92, 255, cv2.THRESH_BINARY)[1]
-    # threshold value image and invert
-    thresh2 = cv2.threshold(v, 128, 255, cv2.THRESH_BINARY)[1]
-    thresh2 = 255 - thresh2
-    # combine the two threshold images as a mask
-    mask = cv2.add(thresh1, thresh2)
-    # use mask to remove lines in background of input
-    result = img.copy()
-    result[mask == 0] = (255, 255, 255)
-    return result
+    @staticmethod
+    def from_str(label):
+        if label in ('geofond1dot22',):
+            return MethodChoice.geofond1dot22
+        elif label in ('colors_replacement',):
+            return MethodChoice.colors_replacement
+        elif label in ('openCV2',):
+            return MethodChoice.openCV2
+        else:
+            raise NotImplementedError
 
 
 def hex_to_rbg(hex_color: str) -> tuple:
@@ -104,6 +77,51 @@ def rgba_to_hex(rgba_color: tuple):
     return '#%02x%02x%02x' % (r, g, b)
 
 
+def replace_images_in_zip(in_zip: Path, out_zip: Path, replacements: Dict[str, io.BytesIO]) -> Path:
+    """
+    Replace images in zip
+    :param in_zip:
+    :param out_zip:
+    :param replacements:
+    :return:
+    """
+
+    with zipfile.ZipFile(in_zip, 'r') as zin:
+        with zipfile.ZipFile(out_zip, 'w') as zout:
+            zout.comment = zin.comment  # preserve the comment
+            for item in zin.infolist():
+                # copy all files except the images to replace
+                if item.filename not in replacements.keys():
+                    with zout.open(item.filename, 'w') as f:
+                        f.write(zin.read(item.filename))
+                else:  # replace the image
+                    with zout.open(item.filename, 'w') as f:
+                        f.write(replacements[item.filename].getvalue())
+    return out_zip
+
+
+def remove_watermark_from_cv_image(img: numpy.ndarray) -> numpy.ndarray:
+    """
+    Remove watermark from open cv image
+    :param img:
+    :return: image without watermark
+    """
+    # convert image to hsv colorspace
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h, s, v = cv2.split(hsv)
+    # threshold saturation image
+    thresh1 = cv2.threshold(s, 92, 255, cv2.THRESH_BINARY)[1]
+    # threshold value image and invert
+    thresh2 = cv2.threshold(v, 128, 255, cv2.THRESH_BINARY)[1]
+    thresh2 = 255 - thresh2
+    # combine the two threshold images as a mask
+    mask = cv2.add(thresh1, thresh2)
+    # use mask to remove lines in background of input
+    result = img.copy()
+    result[mask == 0] = (255, 255, 255)
+    return result
+
+
 def replace_colors_in_image(image: Image, replacements: Dict[str, str]) -> Image:
     """
     Replace colors in image with new colors from replacements {to_replace_hex: new_hex}
@@ -126,6 +144,29 @@ def replace_colors_in_image(image: Image, replacements: Dict[str, str]) -> Image
     return Image.fromarray(data)
 
 
+def remove_watermark_from_pil_image(image: Image, method_choice: MethodChoice,
+                                    replacements: Dict[str, str] = None) -> Image:
+    """
+    Remove watermark from pil image
+    :param image:
+    :param method_choice:
+    :param replacements:
+    :return:
+    """
+    if method_choice == MethodChoice.openCV2:
+        # noinspection PyTypeChecker
+        opencv_image = cv2.cvtColor(numpy.array(image), cv2.COLOR_RGB2BGR)
+        opencv_image = remove_watermark_from_cv_image(opencv_image)
+        image = Image.fromarray(cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB))
+        return image
+    else:
+        return replace_colors_in_image(image, replacements or {
+            '#f0f0f0': '#FFFFFF',
+            '#c0c0c0': '#FFFFFF',
+            '#b4b4fe': '#FFFFFF',
+        })
+
+
 def generate_output_path(input_path: Path) -> Path:
     """
     Generate output_path from input_path
@@ -135,32 +176,74 @@ def generate_output_path(input_path: Path) -> Path:
     return input_path.parent / (input_path.stem + f"_generated{input_path.suffix}")
 
 
-def remove_watermark_from_pdf(input_file: Path, output_file: Path, use_colors_replacement: bool = False) -> str:
+def remove_watermark_from_geofond_pdf(input_file: Path, output_file: Path) -> str:
+    """
+    Remove watermark from pdf (exported from GEOFOND v1.22) and save to output_file.
+    :param input_file:
+    :param output_file:
+    :return:
+    """
+    pdf = Pdf.open(input_file)
+    footer_watermark = None
+    for page_number, page in enumerate(pdf.pages):
+        page_instructions = parse_content_stream(page)
+        # remove VERSION EVALUATION (last f operator)
+        for i in reversed(range(len(page_instructions))):
+            op = page_instructions[i].operator.__str__()
+            if op == 'f':
+                page_instructions[i] = None
+                break
+        page_instructions = [x for x in page_instructions if x is not None]  # remove None
+
+        # remove Trial - \d+ (last TJ with 54 operands)
+        for i in reversed(range(len(page_instructions))):
+            op = page_instructions[i].operator.__str__()
+            if op == 'TJ':
+                instruction = page_instructions[i]
+                if footer_watermark is None:  # first page
+                    if len(instruction.operands[0]) == 54:
+                        footer_watermark = instruction.operands.__str__()
+                        page_instructions[i] = None
+                        break
+                else:  # other pages
+                    if instruction.operands.__str__() == footer_watermark:
+                        page_instructions[i] = None
+                        break
+        page_instructions = [x for x in page_instructions if x is not None]  # remove None
+        new_content_stream = unparse_content_stream(page_instructions)
+        page.Contents = pdf.make_stream(new_content_stream)  # override page contents
+    pdf.save(output_file)
+    return str(output_file)
+
+
+def remove_watermark_from_pdf(input_file: Path, output_file: Path, method_choice: MethodChoice = None) -> str:
     """
     Remove watermark from pdf and save to output_file
     :param input_file:
     :param output_file:
-    :param use_colors_replacement:
+    :param method_choice:
     :return:
     """
+    if method_choice == MethodChoice.geofond1dot22:
+        return remove_watermark_from_geofond_pdf(input_file, output_file)
     pdf = Pdf.open(input_file)
     for page in pdf.pages:
         for image_key in page.images.keys():
             raw_image = page.images[image_key]
             pdf_image = PdfImage(raw_image)
             pil_image = pdf_image.as_pil_image()
-            pil_image = remove_watermark_from_pil_image(pil_image, use_colors_replacement)
+            pil_image = remove_watermark_from_pil_image(pil_image, method_choice)
             raw_image.write(zlib.compress(pil_image.tobytes()), filter=Name("/FlateDecode"))
     pdf.save(output_file)
     return str(output_file)
 
 
-def remove_watermark_from_docx(input_file: Path, output_file: Path, use_colors_replacement: bool = False) -> str:
+def remove_watermark_from_docx(input_file: Path, output_file: Path, method_choice: MethodChoice = None) -> str:
     """
     Remove watermark from docx and save to output_file
     :param input_file:
     :param output_file:
-    :param use_colors_replacement:
+    :param method_choice:
     :return:
     """
     z = zipfile.ZipFile(input_file)
@@ -172,7 +255,7 @@ def remove_watermark_from_docx(input_file: Path, output_file: Path, use_colors_r
         with z.open(image) as f:
             pil_image = Image.open(f)
             # pil_image = improve_text_in_image(pil_image)
-            pil_image = remove_watermark_from_pil_image(pil_image, use_colors_replacement)
+            pil_image = remove_watermark_from_pil_image(pil_image, method_choice)
             image_file_tmp = io.BytesIO()
             pil_image.save(image_file_tmp, format="PNG")
             replacements[image] = image_file_tmp
@@ -180,46 +263,26 @@ def remove_watermark_from_docx(input_file: Path, output_file: Path, use_colors_r
     return str(replace_images_in_zip(input_file, output_file, replacements))
 
 
-def remove_watermark_from_pil_image(image: Image, use_colors_replacement: bool = False) -> Image:
-    """
-    Remove watermark from pil image
-    :param image:
-    :param use_colors_replacement:
-    :return:
-    """
-    if use_colors_replacement:
-        return replace_colors_in_image(image, {
-            '#f0f0f0': '#FFFFFF',
-            '#c0c0c0': '#FFFFFF',
-        })
-    else:
-        # noinspection PyTypeChecker
-        opencv_image = cv2.cvtColor(numpy.array(image), cv2.COLOR_RGB2BGR)
-        opencv_image = remove_watermark_from_cv_image(opencv_image)
-        image = Image.fromarray(cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB))
-        return image
-
-
-def remove_watermark_from_image(input_file: Path, output_file: Path, use_colors_replacement: bool = False) -> str:
+def remove_watermark_from_image(input_file: Path, output_file: Path, method_choice: MethodChoice) -> str:
     """
     Remove watermark from image
     :param input_file:
     :param output_file:
-    :param use_colors_replacement:
+    :param method_choice:
     :return:
     """
     pil_image = Image.open(input_file)
-    pil_image = remove_watermark_from_pil_image(pil_image, use_colors_replacement)
+    pil_image = remove_watermark_from_pil_image(pil_image, method_choice)
     pil_image.save(output_file)
     return str(output_file)
 
 
-def main(input_file: str, output_file: str = None, use_colors_replacement: bool = False) -> str:
+def main(input_file: str, output_file: str = None, method_choice: MethodChoice = None) -> str:
     """
     Entry point
     :param input_file:
     :param output_file:
-    :param use_colors_replacement:
+    :param method_choice:
     :return:
     """
     input_path = Path(input_file)
@@ -235,21 +298,21 @@ def main(input_file: str, output_file: str = None, use_colors_replacement: bool 
         output_path.unlink()
     # remove watermark
     if str(input_path.suffix).lower() == ".pdf":
-        return remove_watermark_from_pdf(input_path, output_path, use_colors_replacement)
+        return remove_watermark_from_pdf(input_path, output_path, method_choice)
     elif str(input_path.suffix).lower() == ".docx":
-        return remove_watermark_from_docx(input_path, output_path, use_colors_replacement)
+        return remove_watermark_from_docx(input_path, output_path, method_choice)
     elif str(input_path.suffix).lower() in [".png", ".jpg", ".jpeg"]:
-        return remove_watermark_from_image(input_path, output_path, use_colors_replacement)
+        return remove_watermark_from_image(input_path, output_path, method_choice)
     else:
         raise Exception(f"Unsupported file type: {input_path.suffix}")
 
 
-def mmain(input_files: List[str], output_dir: str = None, use_colors_replacement: bool = False) -> List[str]:
+def mmain(input_files: List[str], output_dir: str = None, method_choice: MethodChoice = None) -> List[str]:
     """
     Entry point
     :param input_files:
     :param output_dir:
-    :param use_colors_replacement:
+    :param method_choice:
     :return:
     """
     output_files = []
@@ -258,7 +321,7 @@ def mmain(input_files: List[str], output_dir: str = None, use_colors_replacement
         if not output_dir:
             output_dir = input_path.parent
         output_file = output_dir / (input_path.stem + f"_generated{input_path.suffix}")
-        output_file = main(input_file, output_file, use_colors_replacement)
+        output_file = main(input_file, output_file, method_choice)
         output_files.append(output_file)
     return output_files
 
