3fd4091ca2b55b9953d163f48cba074b99fa852a
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2022-02-18 20:27:03 +0100

    refactor debug windows

--- test_scripts/run_win.bat
+++ test_scripts/run_win.bat
@@ -31,6 +31,6 @@ ENDLOCAL
 echo END
 
 Rem cd Desktop\watermark-remover\
-Rem .\test_scripts\run_winn.bat .\scripts\dist\WatermarkRemover.exe 20
-Rem .\test_scripts\run_winn.bat .\scripts\dist\tt.exe 2
-Rem .\test_scripts\run_winn.bat .\scripts\dist\5min.exe 5
+Rem .\test_scripts\run_win.bat .\scripts\dist\WatermarkRemover.exe 20
+Rem .\test_scripts\run_win.bat .\scripts\dist\tt.exe 2
+Rem .\test_scripts\run_win.bat .\scripts\dist\5min.exe 5
