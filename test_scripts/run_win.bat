b19ee78eaca8e010bf2bdaa57bb2857afe74f525
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2022-02-19 13:20:23 +0100

    windows debug

--- test_scripts/run_win.bat
+++ test_scripts/run_win.bat
@@ -8,22 +8,24 @@ echo executable is %EXECUTABLE%
 echo filename is %FILENAME%
 dir
 
-START "" %EXECUTABLE%
+@REM echo Starting %EXECUTABLE%...
+@REM START "" %EXECUTABLE%
 
-echo Waiting for %TIMEOUT% seconds...
-ping 127.0.0.1 -n %TIMEOUT% > NUL
+echo Waiting for %TIMEOUT% seconds..
+timeout /T %TIMEOUT% /NOBREAK > NUL
 
+tasklist /fi "ImageName eq %FILENAME%" /fo csv 2>NUL | find /I "%FILENAME%"> NUL
+echo ERROR LEVEL is %ERRORLEVEL%
 
-tasklist /fi "ImageName eq %FILENAME%" /fo csv 2>NUL | find /I "%FILENAME%">NUL
-
-echo ERROR LEVEL is %ERRORLEVEL% 
 IF %ERRORLEVEL% NEQ 0 (
    echo Program %FILENAME% is not running
+   echo Quitting %FILENAME%
    taskkill /f /im %FILENAME%
    echo failed with %ERRORLEVEL% 
    exit 1
 ) ELSE (
    echo Program %FILENAME% is running
+   echo Quitting %FILENAME%
    taskkill /f /im %FILENAME%
    echo succeed with %ERRORLEVEL%
    exit 0
--- test_scripts/windows_exe_example.sh
+++ test_scripts/windows_exe_example.sh
@@ -6,4 +6,5 @@ cd Desktop\watermark-remover\
 # Rem .\test_scripts\run_win.bat .\scripts\dist\tt.exe 2
 # Rem .\test_scripts\run_win.bat .\scripts\dist\5min.exe 5
 
-# @REM timeout /T %TIMEOUT% /NOBREAK > nul
+# @REM timeout /T %TIMEOUT% /NOBREAK > NUL
+# @REM ping 127.0.0.1 -n %TIMEOUT% > NUL
